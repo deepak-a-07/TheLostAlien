401cfdee6430dec2a5e42766b877e8be70f54742
deepak-a-07 <deepakanand093@gmail.com>
2022-04-09 20:29:55 +0530

    second commit

---
+++ app/src/main/java/com/example/thelostalien/Constants.java
@@ -0,0 +1,14 @@
+package com.example.thelostalien;
+
+
+import android.content.Context;
+
+
+public class Constants {
+    public static int SCREEN_WIDTH;
+    public static int SCREEN_HEIGHT;
+
+    public static Context CURRENT_CONTEXT;
+
+    public static long INIT_TIME;
+}
--- app/src/main/java/com/example/thelostalien/MainActivity.java
+++ app/src/main/java/com/example/thelostalien/MainActivity.java
@@ -2,8 +2,14 @@ package com.example.thelostalien;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,12 +20,42 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN); // get rid of tool bar and set fullscreen
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE); // get rid of toolbar
+
+        DisplayMetrics dm = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(dm);
+        Constants.SCREEN_WIDTH = dm.widthPixels;
+        Constants.SCREEN_HEIGHT = dm.heightPixels;
+
         setContentView(R.layout.activity_main);
+
 
         MainActivity.mediaPlayer = MediaPlayer.create(this,R.raw.music);
         if(MainActivity.mediaPlayer != null) {
             MainActivity.mediaPlayer.setLooping(true);
             MainActivity.mediaPlayer.start();
         }
+
+        ibStartGame = findViewById(R.id.ibStartGame);
+        ibStartGame.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, GamePanel.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+    @Override
+    public void onBackPressed() {
+        if(MainActivity.mediaPlayer != null) {
+            MainActivity.mediaPlayer.stop();
+            MainActivity.mediaPlayer.release();
+            MainActivity.mediaPlayer = null;
+        }
+        super.onBackPressed();
     }
 }
